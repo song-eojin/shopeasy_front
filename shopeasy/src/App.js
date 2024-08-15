49cac29c38c6c718900a0a8b98ff5efe20a731fd
song eojin <feshed528@gmail.com>
2024-08-15 11:10:25 +0900

    Feature : 홈페이지 - 파일 구성 (Issue #1)

--- shopeasy/src/App.js
+++ shopeasy/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+/*리액트 애플리케이션의 주요 컴포넌트를 정의하는 파일로, JSX를 사용하여 애플리케이션의 기본 UI와 로직을 구성하는 리액트 컴포넌트를 정의*/
+
+import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Header />
+      {/*여기에 페이지별 콘텐츠 추가*/}
+      <Footer />
     </div>
   );
 }
--- shopeasy/src/index.js
+++ shopeasy/src/index.js
@@ -1,13 +1,15 @@
+/*리액트 애플리케이션의 진입점으로 ReactDOM.render() 메서드를 사용하여 index.html 파일의 <div id="root"> </div>에 렌더링*/
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App />{/*HTML 태그처럼 보이지만 사실 JSX 구문이므로 주석 형태는 이렇게 해야 하며, <App />은 사용자 정의 컴포넌트*/}
   </React.StrictMode>
 );
 
